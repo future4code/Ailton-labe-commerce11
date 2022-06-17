80aabebb4bc69244f756120d0cd707b545692622
Talita Miguel <talita.leonardo@gmail.com>
2022-06-17 18:31:00 -0300

    estilização

--- src/components/Carrinho/Carrinho.js
+++ src/components/Carrinho/Carrinho.js
@@ -1,23 +1,56 @@
 import React from 'react';
 import styled from 'styled-components'
+import Cancelamento from '../../img/cancelamento.png'
+import Comprar from '../../img/comprar.png'
 
 const CarrinhoContainer = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid purple;
   width: 100%;
-  padding-left: 8px;
-  p{
+  color: white;
+  p {
+    text-align: left;
+    padding: 0 8px;
+  }
+  h4 {
     text-align: left;
   }
-  button{
-    margin: 15px;
+  div {
+    display: flex;
+  }
+`
+const Valor = styled.p`
+  width: 15%;
+`
+
+const BotaoCancelar = styled.button`
+  margin: 15px;
+  background: transparent;
+  border: none;
+  cursor: pointer;
+
+  img {
+    width: 25px;
   }
 `
 
-
+const BotaoComprar = styled.button`
+  background: transparent;
+  border: none;
+  cursor: pointer;
+  img {
+    width: 100px;
+  }
+  &:hover{
+    opacity: 0.5;
+  }
+`
 
 class Carrinho extends React.Component {
+  confirmacaoDeCompra = () => {
+    alert(`Pedido enviado!`)
+  }
+
   valorTotal = () => {
     let totalValor = 0
 
@@ -33,20 +66,18 @@ class Carrinho extends React.Component {
         <div>
           <CarrinhoContainer>
           <h3>Carrinho</h3>
-          <p>Produtos:</p>
+          <h4>Produtos:</h4>
           {this.props.produtosNoCarrinho.map((item, index) => {
             return <div key={index}>
               <p >{item.quantidade}x</p>
               <p >{item.name}</p>
-              <p >{item.value}</p>
-              <button onClick={() => this.props.removeProdutoCarrinho(item.id)}>
-                Remover
-              </button>
+              <Valor >{item.value}</Valor>
+              <BotaoCancelar onClick={() => this.props.removeProdutoCarrinho(item.id)}><img src={Cancelamento} alt='Cancelar Compra'/></BotaoCancelar>
             </div>
           })}
 
-          <p>Valor total: R${this.valorTotal()},00</p>
-          <button>Comprar</button>
+          <h4>Valor total: R$ {this.valorTotal()},00</h4>
+          <BotaoComprar onClick={() => this.confirmacaoDeCompra()}><img src={Comprar} alt='Compra'/></BotaoComprar>
             
           </CarrinhoContainer>
         </div>
